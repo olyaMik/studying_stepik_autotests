b626d81f7c7fc13ca376b4e3ea9b0bb1cf5828d2
olga_m <olyamikhailovanma@gmail.com>
2022-01-24 14:04:18 +0200

    new_commit_24.01.22

---
+++ pages/base_page.py
@@ -0,0 +1,21 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
+class BasePage():
+    def __init__(self, driver, url, timeout=10):
+        self.driver = driver
+        self.url = url
+        self.driver.implicitly_wait(timeout)
+
+    def get_page(self):
+        self.driver.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,14 @@
+from .pages.main_page import MainPage
 from selenium import webdriver
 
-
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(driver, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.get_page()                      # открываем страницу
+    page.go_to_login_page()
+
+def test_guest_should_see_login_link(driver):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(driver, link)
+    page.get_page()
+    page.should_be_login_link()
